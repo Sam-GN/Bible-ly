fa0828fa51bd22f99ef7e188f0dbedcc339d22b6
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2019-10-16 23:17:19 -0700

    Revert "adding utils"
    This reverts commit 65872003575390df247eafeb49ace027ea712f44.

--- app/src/main/java/edu/calbaptist/bible_ly/MainActivity.kt
+++ app/src/main/java/edu/calbaptist/bible_ly/MainActivity.kt
@@ -15,40 +15,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import com.google.firebase.database.*
-import com.google.firebase.firestore.FirebaseFirestore
-import edu.calbaptist.bible_ly.util.ClassUtil
-import edu.calbaptist.bible_ly.util.EventUtil
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var database: FirebaseFirestore
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        database = FirebaseDatabase.getInstance().reference
 
-        // Enable Bible-ly Logging
-        FirebaseFirestore.setLoggingEnabled(true)
 
-        // Bible-ly
-        database = FirebaseFirestore.getInstance()
 
-        // commented code below. fixing database
-//        // the below code is for firebase testing purposes
-//        database.child("users").addValueEventListener(object : ValueEventListener {
-//
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//               /// val userr = snapshot as User
-//                val userr =snapshot.children.mapNotNull { it.getValue<User>(User::class.java) }
-//                Log.v("rrr",userr[0].userName)
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {}
-//        })
+        // the below code is for firebase testing purposes
+        database.child("users").addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+               /// val userr = snapshot as User
+                val userr =snapshot.children.mapNotNull { it.getValue<User>(User::class.java) }
+                Log.v("rrr",userr[0].userName)
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
 //        val postListener = object : ValueEventListener {
 //            override fun onDataChange(dataSnapshot: DataSnapshot) {
 //                // Get Post object and use the values to update the UI
@@ -65,17 +58,17 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        database.child("users").addValueEventListener(postListener)
 
-//        val fab: FloatingActionButton = findViewById(R.id.fab)
-//
-//
-//        fab.setOnClickListener { view ->
-//            // Write a message to the database
-//
-//// ...
-//
-//            val user = User(1, "Sam","123","")
-//            database.child("users").child("1").setValue(user)
-//        }
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+
+        fab.setOnClickListener { view ->
+            // Write a message to the database
+
+// ...
+
+            val user = User(1, "Sam","123","")
+            database.child("users").child("1").setValue(user)
+        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -101,34 +94,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    private fun onAddItemsClicked() {
-        // Add a bunch of random restaurants
-        val batch = database.batch()
-        for (i in 0..9) {
-            val restRef = database.collection("classes").document()
-
-            // Create random events / classes
-            val randomClass = ClassUtil.getRandom(this)
-            val randomRatings = EventUtil.getRandomList(randomClass.numEvents)
-            randomRestaurant.avgRating = RatingUtil.getAverageRating(randomRatings)
-
-            // Add restaurant
-            batch.set(restRef, randomRestaurant)
-
-            // Add ratings to subcollection
-            for (rating in randomRatings) {
-                batch.set(restRef.collection("ratings").document(), rating)
-            }
-        }
-
-        batch.commit().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.d(TAG, "Write batch succeeded.")
-            } else {
-                Log.w(TAG, "write batch failed.", task.exception)
-            }
-        }
     }
 }
